9048b25cdb9ad975e6f43bd5e30cd60101a50ba9
Heather King <hkingdevelopment@gmail.com>
2021-10-13 16:36:44 +0100

    adding progress

--- start_code/task_list.py
+++ start_code/task_list.py
@@ -7,23 +7,79 @@ tasks = [
 ]
 
 #task 1
-task_list = []
-index = 0
 
-for task in tasks:
-    if tasks[index]["completed"] == False:
-        task_list.append(tasks[index]["description"])
-    index += 1
+def uncompleted_tasks(list):
+    task_list = []
+    index = 0
+    for item in list:
+        if list[index]["completed"] == False:
+            task_list.append(list[index]["description"])
+        index += 1
     
-print(task_list)
+    print(task_list)
+
+uncompleted_tasks(tasks)
 
 #task 2
-task_list = []
-index = 0
 
-for task in tasks:
-    if tasks[index]["completed"] == True:
-        task_list.append(tasks[index]["description"])
-    index += 1
+def completed_tasks(list):
+    task_list = []
+    index = 0
+
+    for task in list:
+        if list[index]["completed"] == True:
+            task_list.append(list[index]["description"])
+        index += 1
     
-print(task_list)
+    print(task_list)
+
+completed_tasks(tasks)
+
+
+# # 3. Print a list of all task descriptions
+
+def task_descriptions(list):
+    index = 0
+
+    for item in list:
+        print(list[index]["description"])
+        index += 1
+
+task_descriptions(tasks)
+
+# # 4. Print a list of tasks where time_taken is at least a given time
+
+
+def time_taken(list, min_time):
+    index = 0
+
+    for item in list:
+        if list[index]["time_taken"] >= min_time:
+            print(list[index]["description"])
+        index += 1
+
+time_taken(tasks, 15)
+
+# 5. Print any task with a given description
+
+def given_description(list, description):
+    index = 0
+
+    for item in list:
+        if list[index]["description"] == description:
+           print(list[index]) 
+        index += 1
+
+given_description(tasks, "Feed Cat")
+
+#6. Given a description update that task to mark it as complete.
+
+def status_update(list, description):
+    index = 0
+
+    for item in list:
+        if list[index]["description"] == description:
+            list[index]["completed"] = True
+        index += 1
+
+print(status_update(tasks, "Feed Cat"))
